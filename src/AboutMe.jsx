dbe5a55b538dde557173579fa5c5dbe4b38829ef
kjavan <k.javan@gmx.de>
2026-03-17 15:11:29 +0100

    update 2

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import image from "/src/pics/MyPic.jpeg";
 import image2 from "/src/pics/Gestaltung.png";
+import image3 from "/src/pics/BaPic.png";
+import image4 from "/src/pics/BWL.png";
 import pdf from "/src/pdfs/Gestaltung.pdf";
+import pdf2 from "/src/pdfs/BA.pdf";
+import pdf3 from "/src/pdfs/BWL.pdf";
+
 
 const AboutMe = ({ onBack }) => {
 
@@ -10,7 +15,7 @@ setshowpdf ist der Funktion
 showPdf → speichert den Zustand
 setShowPdf → ändert den Zustand  
 wenn man den Button drückt, dann wird die setShowpdf auf true gesetzt und öffnet das PDF und showpdf wird auch true weil er der Zustand speichert*/
-const [showPdf, setShowPdf] = useState(false);
+const [selectedPdf, setSelectedPdf] = useState(null);
 
 const titel = "Mein Name ist Khashayar Javan ";
 const text1 = "Ich bin 26 Jahre alt und komme ursprünglich aus dem Iran. Seit 2014 lebe ich in Deutschland, genauer gesagt in Hamburg.";
@@ -36,9 +41,29 @@ const gest1 = [
 ] ;
 
 
+const baTitel = "Darstellung des Bildes der Bachelorarbeit";
+const ba1 = [
+  "Virtual Reality findet zunehmend Anwendung in unterschiedlichen gesellschaftlichen und wirtschaftlichen Bereichen. Durch das hohe Maß an Immersion kann die Technologie Wahrnehmungsprozesse gezielt beeinflussen. Insbesondere die subjektive Zeitwahrnehmung kann sich innerhalb virtueller Umgebungen vom objektiven Zeitverlauf unterscheiden. Während zahlreiche Studien Faktoren wie Immersion, Interaktion oder Embodiment untersucht haben, wurde der Einfluss situativer Rahmenbedingungen, insbesondere der Tageszeit, bislang kaum berücksichtigt.", 
+  "Vor diesem Hintergrund wurde in der vorliegenden Bachelorarbeit die Forschungsfrage untersucht, welchen Einfluss die Tageszeit auf die Genauigkeit der subjektiven Zeiteinschätzung innerhalb einer Virtual-Reality-Umgebung hat. Hierzu wurde ein experimentelles Studiendesign mit 18 Teilnehmenden durchgeführt, die zu zwei unterschiedlichen Tageszeiten jeweils ein VR-Spiel absolvierten und im Anschluss die Dauer der Aufgabe eingeschätzten.",
+  "Die Ergebnisse zeigen nur geringe Unterschiede in der mittleren Abweichung zwischen geschätzter und tatsächlich gemessener Zeit. Ein deutliche Veränderung der Genauigkeit der Zeiteinschätzung zwischen den beiden Tageszeiten konnte nicht festgestellt werden. Trotz leichter Unterschiede im zweiten Durchgang ließ sich kein klarer Einfluss der Tageszeit nachweisen.",
+]
+
+
+const BWLTitel = "Darstellung des Bildes der BWL";
+const BWL1 = [
+  "Im Rahmen meines Studiums der Medieninformatik habe ich auch betriebswirtschaftliche Kenntnisse erworben, die für die Entwicklung und Vermarktung von Medienprodukten von großer Bedeutung sind. In diesem Abschnitt möchte ich meine Erfahrungen und Erkenntnisse im Bereich Betriebswirtschaftslehre (BWL) teilen.", 
+  "Während meines Studiums habe ich das Module BWL belegt, die darunter um die Marketing, Projektmanagement, Unternehmensführung ging. Diese Kurse haben mir ein grundlegendes Verständnis für betriebswirtschaftliche Konzepte vermittelt und mir geholfen, die wirtschaftlichen Aspekte von Medienprojekten besser zu verstehen.",
+  "Ein wichtiger Aspekt der BWL ist das Thema \"Datensicherung aber wie?\"",
+  "Dazu habe ich eine 20 Minütige Präsentation erstellt, die die verschiedenen Arten der Datensicherung erläutert, darunter Vollsicherung, inkrementelle Sicherung und differentielle Sicherung. In meiner Präsentation habe ich die Vor- und Nachteile jeder Methode diskutiert und Beispiele für deren Anwendung gegeben.",
+]
+
+
+
+
+
 return (
 <>
-    
+{/* hier wird die Seite Persönliche infos,*/}    
 <div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
   <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
 
@@ -67,6 +92,39 @@ return (
 </div>
 
 
+
+
+{/* hier wird die Seite BA,*/} 
+<div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
+  <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
+
+    <h1 className="text-2xl font-semibold mb-4">Bachelorarbeit</h1>
+
+    <img src={image3} alt="Gestaltung" className="mx-auto mb-6 rounded"/>
+
+    <h2 className="text-gray-300 mb-6">{baTitel}</h2>
+
+      {ba1.map((text, index) => (
+        <p key={index} className="mb-4">
+         {text}
+         </p>
+      ))}
+      
+      <button
+        onClick={() => setSelectedPdf(pdf2)}
+        className="bg-white text-black px-4 py-2 rounded my-4"
+      >
+        PDF anschauen
+      </button>
+    
+    
+
+  </div>
+</div>
+
+
+
+{/* hier wird die Seite Gestaltung,*/} 
 <div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
   <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
 
@@ -76,37 +134,87 @@ return (
 
     <h2 className="text-gray-300 mb-6">{gestTitel}</h2>
 
-    <ul className="text-gray-300 mb-4 list-disc pl-6">
-      <p>{gest1[0]}</p>
-      <p>{gest1[1]}</p>
-      <p>{gest1[2]}</p>
-      <p>{gest1[3]}</p>
-      <p>{gest1[4]}</p>
-
+   {gest1.map((text2, index2) => (
+        <p key={index2} className="mb-4">
+         {text2}
+         </p>
+      ))}
 
       <button
-        onClick={() => setShowPdf(true)}
+        onClick={() => setSelectedPdf(pdf)}
         className="bg-white text-black px-4 py-2 rounded my-4"
-      >
+    >
         PDF anschauen
       </button>
 
     
-    </ul>
+   
 
   </div>
 </div>
 
 
+
+{/* hier wird die Seite BWL,*/} 
+<div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
+  <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
+
+    <h1 className="text-2xl font-semibold mb-4">Betriebswirtschaftslehre (BWL)</h1>
+
+    <img src={image4} alt="Gestaltung" className="mx-auto mb-6 rounded"/>
+
+    <h2 className="text-gray-300 mb-6">{BWLTitel}</h2>
+
+    {BWL1.map((text, index) => (
+        <p key={index} className="mb-4">
+         {text}
+         </p>
+      ))}
+      <button
+        onClick={() => setSelectedPdf(pdf3)}
+        className="bg-white text-black px-4 py-2 rounded my-4"
+    >
+        PDF anschauen
+      </button>
+   
+  </div>
+</div>
+
+
+<div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
+    <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
+       <h1 className="text-2xl font-semibold mb-4">Betriebswirtschaftslehre (BWL)</h1>
+
+    <img src={image4} alt="Gestaltung" className="mx-auto mb-6 rounded"/>
+
+    <h2 className="text-gray-300 mb-6">{BWLTitel}</h2>
+
+    {BWL1.map((text, index) => (
+        <p key={index} className="mb-4">
+         {text}
+         </p>
+      ))}
+      <button
+        onClick={() => setSelectedPdf(pdf3)}
+        className="bg-white text-black px-4 py-2 rounded my-4"
+    >
+        PDF anschauen
+      </button>
+
+    </div>
+</div>
+
+
+
 {/* wenn PDF true ist dann exisirtiert ein Button um das zu schließen */}
-{showPdf && (
+{selectedPdf && (
   <div className="fixed inset-0 bg-black bg-opacity-70 flex items-center justify-center z-50">
 
     <div className="bg-white p-4 rounded shadow-lg w-[80%] h-[80%] flex flex-col">
 
       <div className="flex justify-end mb-2">
         <button
-          onClick={() => setShowPdf(false)}
+          onClick={() => setSelectedPdf(null)}
           className="bg-red-500 text-white px-3 py-1 rounded"
         >
           Schließen
@@ -114,7 +222,7 @@ return (
       </div>
 
       <iframe
-        src={pdf}
+        src={selectedPdf}
         className="w-full h-full rounded"
         title="PDF Viewer"
       />
@@ -125,35 +233,7 @@ return (
 )}
 
 
-<div className="min-h-screen flex items-center justify-center p-4 bg-gray-900 text-white">
-  <div className="max-w-2xl p-6 bg-gray-800 rounded shadow-lg">
 
-    <h1 className="text-2xl font-semibold mb-4">Bachelorarbeit</h1>
-
-    <img src={image2} alt="Gestaltung" className="mx-auto mb-6 rounded"/>
-
-    <h2 className="text-gray-300 mb-6">{gestTitel}</h2>
-
-    <ul className="text-gray-300 mb-4 list-disc pl-6">
-      <p>{gest1[0]}</p>
-      <p>{gest1[1]}</p>
-      <p>{gest1[2]}</p>
-      <p>{gest1[3]}</p>
-      <p>{gest1[4]}</p>
-
-
-      <button
-        onClick={() => setShowPdf(true)}
-        className="bg-white text-black px-4 py-2 rounded my-4"
-      >
-        PDF anschauen
-      </button>
-
-    
-    </ul>
-
-  </div>
-</div>
 
 
 
